4c9a3ce6d8cda12f4f15adaea8b15938c46de9ec
Steven Nance <stevenmnance@gmail.com>
2020-04-18 14:47:38 -0700

    suffix UI components with UI

--- Lib/MovieBrowserUI/MovieBrowserUI.py
+++ Lib/MovieBrowserUI/MovieBrowserUI.py
@@ -1,12 +1,12 @@
-class MovieBrowser:
+class MovieBrowserUI:
     def __init__(self, ownerComponent):
         self.ownerComponent = ownerComponent
-        print("Movie browser initializing")
 
     def GetMovie(self, movieCompName):
         comp = self.ownerComponent.op(movieCompName)
         assert (
             comp is not None
         ), 'requested movie path for unknown movie comp: "{}"'.format(movieCompName)
+
         data = comp.op("./select_video")
-        return (data[1, "basename"], data[1, "relpath"])
+        return (data[1, "basename"].val, data[1, "relpath"].val)
