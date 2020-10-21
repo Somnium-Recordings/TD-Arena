8187bb15c56175b9fb2b335b1711b1ac9881f315
Steven Nance <stevenmnance@gmail.com>
2020-10-20 21:45:22 -0700

    select functionality for decks
    Closes #29

--- Lib/Render/CompositionCtrl/CompositionCtrl.py
+++ Lib/Render/CompositionCtrl/CompositionCtrl.py
@@ -3,7 +3,8 @@ TODO: this would probably be much simpler with an FSM...
 Or at least some sort of Ctrl base class
 """
 from tda import LoadableExt
-from tdaUtils import clearChildren, getDeckID, getLayerID, layoutComps
+from tdaUtils import (clearChildren, getCellValues, getDeckID, getLayerID,
+                      layoutComps)
 
 
 class CompositionCtrl(LoadableExt):
@@ -11,6 +12,7 @@ class CompositionCtrl(LoadableExt):
 		super().__init__(ownerComponent, logger)
 		self.selectPrevis = ownerComponent.op('../select_previs')
 		self.compositionContainer = ownerComponent.op('../composition')
+		self.compositionState = ownerComponent.op('./null_compositionState')
 
 		self.Init()
 
@@ -19,24 +21,40 @@ class CompositionCtrl(LoadableExt):
 		self.clearCompositionContents()
 		self.layoutCompositionContainer()
 
-		# TODO: Set defaults on compositionContainer
-
+		self.setDefaultState()
 		self.logInfo('initialized')
+
+	def setDefaultState(self):
+		for parameter in self.compositionContainer.customPars:
+			parameter.val = parameter.default
 
 	def Load(self, state=None):  # pylint: disable=unused-argument
 		self.setLoading()
 		self.logInfo('loading composition')
 
-		# TODO: process state
-		# TODO: set values from state on compositionContainer
+		if state is None:
+			self.setDefaultState()
+		else:
+			for (config, value) in state:
+				par = getattr(self.compositionContainer.par, config)
+				if par is not None:
+					par.val = value
+					self.logDebug('setting coomposition {} to {}'.format(config, value))
+				else:
+					self.logWarning(
+						'could not set composition config for unkown name {}'.format(config)
+					)
+
 		# TODO: set previs selection from state? (can we derive from prop comCtrl prop instead)
 
 		self.setLoaded()
 
 		self.logInfo('loaded')
 
-	def GetSaveState(self):  # pylint: disable=no-self-use
-		return {}
+	def GetSaveState(self):
+		return [
+			getCellValues(config) for config in self.compositionState.rows()[1:]
+		] if self.Loaded else None
 
 	def clearCompositionContents(self):
 		self.logInfo('clearing composition container')
