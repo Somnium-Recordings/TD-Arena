441e539b72d203351ac8c6a1b49758e39998d60f
Steven Nance <stevenmnance@gmail.com>
2021-01-02 19:10:32 -0800

    generic drag/drop controller

--- Lib/UI/DragCtrl/DragCtrl.py
+++ Lib/UI/DragCtrl/DragCtrl.py
@@ -1,9 +1,15 @@
-from tda import BaseExt
+from tda import BaseExt, Par
 
 
-class UI(BaseExt):
+class DragCtrl(BaseExt):
 	def __init__(self, ownerComponent, logger) -> None:
 		super().__init__(ownerComponent, logger)
+
+		self.IsDragging: bool
+		self._IsDragging: Par[bool]
+
+		TDF = op.TDModules.mod.TDFunctions
+		TDF.createProperty(self, 'IsDragging', value=False, readOnly=True)
 
 	# dropName: dropped node name or filename
 	# [x/y]Pos: position in network pane
@@ -15,7 +21,8 @@ class UI(BaseExt):
 	def OnDrop(
 		self, dropName, xPos, yPos, index, totalDragged, dropExt, baseName, destPath
 	):  # pylint: disable=too-many-arguments
-		pass
+		print(f'dropped on : {destPath}')
+		print(f'texture is {op(destPath + "/null_state")["texture"]}')
 
 	#
 	#  drag arguments for nodes          (and files)
@@ -26,4 +33,13 @@ class UI(BaseExt):
 	#       arg4 operator                     (or file extension)
 	#       arg5 dragged node parent network  (or parent directory)
 	def OnDrag(self, dragName, index, num, dragExt, baseName):  # pylint: disable=too-many-arguments,unused-argument,no-self-use
+		self._IsDragging.val = True
 		print('dragging')
+
+	def OnDragRelease(self):
+		self._IsDragging.val = False
+		print('drag released')
+
+	def DragAction(self, u, v, controls):
+		print(f'checking action for {controls.path}')
+		return 1 if u > .5 else 0
