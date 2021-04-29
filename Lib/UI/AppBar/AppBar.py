6094f49175f01c02e95dedc8dd6df60fcccf1514
Steven Nance <stevenmnance@gmail.com>
2021-04-28 21:58:01 -0700

    minimize and maximize through win32 apis

--- Lib/UI/AppBar/AppBar.py
+++ Lib/UI/AppBar/AppBar.py
@@ -1,22 +1,6 @@
-import ctypes
-
 from tda import BaseExt, Par
-
-# These SW_ constants are used for ShowWindow() and are documented at
-# https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-showwindow#parameters
-SW_FORCEMINIMIZE = 11
-SW_HIDE = 0
-SW_MAXIMIZE = 3
-SW_MINIMIZE = 6
-SW_RESTORE = 9
-SW_SHOW = 5
-SW_SHOWDEFAULT = 10
-SW_SHOWMAXIMIZED = 3
-SW_SHOWMINIMIZED = 2
-SW_SHOWMINNOACTIVE = 7
-SW_SHOWNA = 8
-SW_SHOWNOACTIVATE = 4
-SW_SHOWNORMAL = 1
+from win32 import (findWindowByName, isMaximizedWindow, minimizeWindow,
+                   toggleMaximizeWindow)
 
 
 class AppBar(BaseExt):
@@ -30,20 +14,17 @@ class AppBar(BaseExt):
 		TDF.createProperty(self, 'Maximized', value=False)
 
 	@property
-	def window(self):
-		return ctypes.windll.user32.FindWindowW(None, 'TD Arena')
+	def uiWindow(self):
+		return findWindowByName('TD Arena')
 
-	def Minimize(self):
+	def OnUIResize(self):
+		print('resized')
+		self.Maximized = isMaximizedWindow(self.uiWindow)
+
+	def MinimizeUI(self):
 		print('minimizing')
-		ctypes.windll.user32.ShowWindow(self.window, SW_MINIMIZE)
+		minimizeWindow(self.uiWindow)
 
-	def ToggleMaximize(self):
-		# NOTE: I'm not using GetWindowPlacement because it's too much of a pain
-		#       to create a WindowPlacement struct to access showCmd
-		if self.Maximized:
-			print('restoring')
-			ctypes.windll.user32.ShowWindow(self.window, SW_RESTORE)
-		else:
-			print('maximizing')
-			ctypes.windll.user32.ShowWindow(self.window, SW_MAXIMIZE)
-			run('args[0].Maximized = True', self, delayFrames=10)
+	def ToggleMaximizeUI(self):
+		print('toggling')
+		toggleMaximizeWindow(self.uiWindow)
