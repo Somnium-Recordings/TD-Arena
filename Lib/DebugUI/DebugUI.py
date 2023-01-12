2556ef4d1e64e05f95e75079ed20278ca8fd96b5
Steven Nance <stevenmnance@gmail.com>
2023-01-12 22:58:58 +0100

    fix cook loop in debugUI, add clear button for search filter

--- Lib/DebugUI/DebugUI.py
+++ Lib/DebugUI/DebugUI.py
@@ -1,12 +1,12 @@
 class DebugUI:
 	@property
 	def IsOpen(self):
-		return self.windowInfo['winopen']
+		return self.WindowInfo['winopen']
 
 	def __init__(self, ownerComp) -> None:
 		self.ownerComp = ownerComp
 		self.window = ownerComp.op('window_debug')
-		self.windowInfo = ownerComp.op('info_window')
+		self.WindowInfo = ownerComp.op('info_window')
 
 	def Open(self):
 		if not self.IsOpen:
